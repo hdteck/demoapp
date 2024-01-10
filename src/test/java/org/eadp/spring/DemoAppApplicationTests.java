6fbf9d4f670693daa2986ed2cd0633b0a90b4739
hdteck <45236600+hdteck@users.noreply.github.com>
2024-01-10 15:31:18 +0100

    subida config

---
+++ src/test/java/org/eadp/spring/DemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.eadp.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
